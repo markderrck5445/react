2c447e89e76c92bb3810d6ebc2df26966305636f
markderrck5445 <nguyagwamarkderrick@gmail.com>
2025-04-12 13:19:49 +0300

    added new files ,pages ,navbar,and home section added some content in it

--- project1/src/components/NavBar.js
+++ project1/src/components/NavBar.js
@@ -3,14 +3,6 @@ import React from 'react'
 const NavBar=()=> {
   return (
     <div>
-     {/* <nav className="nav">
-          <ul>
-            <li><a href="admin">Admin</a></li>
-            <li><a href="blog">Blog</a></li>
-            <li><a href="/contact">Services</a></li>
-          </ul>
-      </nav>  */}
-
       <nav className="navbar">
         <ul>
           <li><a href="/">Home</a></li>
--- project1/src/pages/Home.js
+++ project1/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 const Home=()=> {
   return (
     <div>
+      <div className="container">
      <div className="col-1">
         <h1 className="shilla">
        <span> I am </span>
@@ -11,6 +12,7 @@ const Home=()=> {
         Welcome to my official website, a hub dedicated to creativity, storytelling, and personal growth. , and I am a writer, journalist, and creative strategist with a passion for exploring the power of words.
         This platform serves as a space where I share my original poetry, thought-provoking articles, and insights into Creative Voice Mastery, a framework designed to help individuals unlock their full creative potential.
         </p>
+     </div>
      </div>
     </div>
     
